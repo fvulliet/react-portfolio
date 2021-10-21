ef267eb750af0f910c6d254fad60ccdb178744dc
Frederic Vulliet <frederic.vulliet@somfy.com>
2021-10-21 10:59:52 +0200

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import Lottie from 'lottie-web';
+import animate from 'lottie-web';
+
+const Hero = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./creativity.json'),
+    });
+    animate.setSpeed(0.5);
+  }, []);
+
+  return (
+    <section className='my-16 px-5 md:my-32 flex flex-col flex-wrap space-y-16 md:space-y-0 patterns'>
+      <div className='ml-auto w-72 md:w-96 lg:w-2/5'>
+        <div className='container' ref={container}></div>
+      </div>
+      <div className='max-w-md mb-auto space-y-5'>
+        <h1 className='text-5xl font-bold md:text-7xl'>Hello. I’m Frédéric</h1>
+        <p className='tracking-wide leading-relaxed'>
+          Curiousity, commitment, and creativity set in my backpack while i’m tripping accross life… I also carry a strong technical background in software engineering, and a growing interest in UX and UI design. The journey is nicer when you meet people, so i’m always glad to share my experience with anybody interested.{' '}
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/projectData.js
@@ -0,0 +1,49 @@
+const projects = [
+  {
+    title: 'SOMFY - animeo pro',
+    image: './images/animeoPro.jpg',
+    description: '[in progress] a desktop/web/app application dedicated to facility managers and professional installers, to help them managing entire buildings with automation',
+    tools: ['Qt', 'QML', 'UI design', 'design system'],
+    link: 'https://www.behance.net/gallery/129732509/animeo-Pro'
+  },
+  {
+    title: 'SOMFY - animeo Connect OS',
+    image: './images/connectOS_landing.jpg',
+    description: 'a desktop application to manage fully automated buildings, in terms of comfort, security, and performance; this application controls all the built-in Somfy devices, configure all the parameters and allow the supervizion of each single zone of the building, but also full levels and facades.',
+    tools: ['Figma', 'UI design', 'design system'],
+    link: 'https://www.behance.net/gallery/129319731/animeo-Connect'
+  },
+  {
+    title: 'La Cave Aux Amis',
+    image: './images/caveauxamis.jpg',
+    description: 'logos, communication, website and advertisment campaigns',
+    tools: ['Illustrator', 'Photoshop', 'Inkscape'],
+    link: 'https://www.behance.net/gallery/129321275/La-Cave-Aux-Amis',
+  },
+  // {
+  //   title: 'MERN eCommerce',
+  //   image: './images/rn-mockup',
+  //   description: 'Full featured ecommerce store built with MERN stack and Paypal payments API. Admin page to manage customers, products & orders.',
+  //   tools: ['React', 'Redux', 'MongoDB', 'JWT', 'Express'],
+  //   github: 'https://github.com/Deelip7/react-node-ecommerce',
+  //   link: 'https://rn-store.herokuapp.com/',
+  // },
+  // {
+  //   title: 'Movie App',
+  //   image: './images/tmdb-mockup',
+  //   description: 'Discover the most popular, top rated and latest movies. Users can also find a movie based on the movie title.',
+  //   tools: ['React', 'API', 'SASS'],
+  //   github: 'https://github.com/Deelip7/react-tmdb-app',
+  //   link: 'https://festive-wilson-a51c32.netlify.app/',
+  // },
+  // {
+  //   title: 'Recipe Finder',
+  //   image: './images/recipeApp-mockup',
+  //   description: 'Browse recipes from a variety of sources. Users can save recipes to view them later. API from themealdb.com.',
+  //   tools: ['Javascript', 'API'],
+  //   github: 'https://github.com/Deelip7/RecipesFinder',
+  //   link: 'https://practical-archimedes-a61091.netlify.app/',
+  // },
+];
+
+export default projects;
